b9084bf4569fbf0adbd8df0c44d86162e9aa3473
lukes611 <lukes611@gmail.com>
2016-04-01 19:46:35 +1000

    saving

--- test2.c
+++ test2.c
@@ -1,23 +1,9 @@
-int ddd = 60;
 
-if(ddd < 100){ 
-	if(ddd < 50) ddd += 4;
-}
+
 
 /*
-if(a) b
-else if(c) d
-else e
 
-ifngoto a l1
-b
-goto l0
-label l1
-ifngoto c l2
-d
-goto l0
-label l2
-e
-label l0
+for(){}
+while(){}
 
 */
