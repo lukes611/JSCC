1f609e45c567baee661ae12a84eed51391cddf00
lukes611 <lukes611@gmail.com>
2016-04-04 07:37:56 +1000

    saving

--- test.js
+++ test.js
@@ -1,34 +1,12 @@
-
-function X(){
-	this.ar = [];
-	for(var i = 0; i < arguments.length; i++) this.ar.push(arguments[i]);
+function BSort(ar){
+	if(ar.length == 0) return [];
+	var rv = ar.reduce(function(p, c){
+		if(p.length == 0) return [c];
+		var last = p.splice(-1)[0];
+		return p.concat( last < c ? [last, c] : [c, last] );
+	}, []);
+	return BSort(rv.slice(0,-1)).concat(rv.slice(-1));
 }
-X.prototype.toString = function(){return this.ar.reduce(function(p,c){return p + ',' + c;});};
-X.prototype.iter = function(){
-	var i = 0, v, m = this;
-	return {
-		hasNext : function(){
-			return i < m.ar.length;
-		},
-		next : function(){
-			return m.ar[i++];
-		},
-		reset: function(){i=0;},
-		valueOf : function(){
-			return m.ar[i];
-		}
-	};
-};
-
-var x = new X(5,6,7,8,9,10);
-console.log(''+x);
-
-var i = x.iter();
-while(i.hasNext()){
-	console.log('GOD: ' + i.next());
-}
-i.reset();
-while(i.hasNext()){
-	console.log(0+i);
-	console.log('DOG: ' + i.next());
-}
+var ar = Array.apply(undefined, Array(5)).map(Math.random).map(function(x){return x*100;}).map(Math.round);
+console.log(ar);
+console.log(BSort(ar));
