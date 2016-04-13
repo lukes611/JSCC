fe64c58fff9db5b7a63ae60873ab666eed95dd91
luke lincoln <lukes611@gmail.com>
2016-04-13 13:35:06 +1000

    s

--- test2.c
+++ test2.c
@@ -1,17 +1,16 @@
 
-int add(int a, int b){
-	return a + b;
-}
-
-int add(int a, int b, int c){
-	return a+b+c;
+void dotProduct(double x, double y, double z, double a, double b, double c, double * out){
+	double tmp = x*a + y*b + z*c;
+	*out = tmp;
 }
 
 int main(int argc, char ** argv){
-	int x = 10;
-	int y = x + 10;
+	double x, y, z;
+	x = 1.0, y = 0.0, z = 0.0;
+	double a,b,c;
+	a = 0.0, b= 1.0, c= 0.0;
+	double out;
 
-
-	return 0;
+	return 0.0;
 	
 }
